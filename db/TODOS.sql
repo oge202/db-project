0718807ef5eccba1de49ba2f026a1c093e6ce879
oge202 <oge.benson@mng.ch>
2025-12-31 12:27:04 +0100

    Hotel, zimmer und kunde

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -1,13 +1,24 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(250) NOT NULL UNIQUE,
-    password VARCHAR(250) NOT NULL
+CREATE TABLE Hotel (
+    Adresse INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(250) NOT NULL,
+    Events VARCHAR(250) NOT NULL
 );
 
-CREATE TABLE todos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    content VARCHAR(100),
-    due DATETIME,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE Zimmer ( 
+    Zimmer_Nr INT AUTO_INCREMENT PRIMARY KEY,
+    Anzahl_Gäste INT,
+    Preis_pro_Nacht NUMERIC(7,2),
+    Zimmer_Art VARCHAR(150),
+    Anzahl_Nächte INT,
+    FOREIGN KEY (hotel_adresse) REFERENCES Hotel(Adresse)
+);
+
+CREATE TABLE Kunde (
+    kunden_id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100),
+    Kreditkarteninformation VARCHAR (100) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+
+
+
 );
