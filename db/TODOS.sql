1fe5a15b375cdacb621e3cd2b6ae11c1e08b7794
oge202 <oge.benson@mng.ch>
2025-12-31 12:46:08 +0100

    Hotel ,Zimmer, Kunde und MItarbeiter

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -15,10 +15,17 @@ CREATE TABLE Zimmer (
 
 CREATE TABLE Kunde (
     kunden_id INT AUTO_INCREMENT PRIMARY KEY,
+    Handynummer VARCHAR(13) NOT NULL UNIQUE
     Name VARCHAR(100),
     Kreditkarteninformation VARCHAR (100) NOT NULL UNIQUE,
-    email VARCHAR(50) NOT NULL UNIQUE,
-
-
-
+    email VARCHAR(50) NOT NULL UNIQUE
 );
+
+CREATE TABLE Mitarbeiter (
+    Mitarbeiter_id INT AUTO_INCREMENT PRIMARY KEY
+    Name VARCHAR (50),
+    Rolle VARCHAR(100)
+    FOREIGN KEY (hotel_adresse) REFERENCES Hotel(Adresse)
+);
+
+CREATE TABLE Restaurant
